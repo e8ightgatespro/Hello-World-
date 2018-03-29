aa1c1a29ee73f84985ed929ebc16d9f2b5fcdb71
e8ightgatespro <garrettvoss4@gmail.com>
2018-03-29 14:34:24 -0500

    Implemented "Hello " + name output when the button is pressed on the screen

--- Hello World!/ViewController.swift
+++ Hello World!/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    @IBAction func showOuput(_ sender: Any) {
+        let name = txtName.text
+        let output = "Hello " + name!
+        lblOutput.text = output
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
